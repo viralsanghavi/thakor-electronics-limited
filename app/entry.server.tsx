de9dd099bf55971101bb45990fc60410dfe1f2fe
viralsanghavi <vsanghavi3@gmail.com>
2024-12-28 22:36:43 +0530

    feat: thakor electronics setup

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -4,13 +4,13 @@
  * For more information, see https://remix.run/file-conventions/entry.server
  */
 
-import { PassThrough } from "node:stream";
+import {PassThrough} from "node:stream";
 
-import type { AppLoadContext, EntryContext } from "@remix-run/node";
-import { createReadableStreamFromReadable } from "@remix-run/node";
-import { RemixServer } from "@remix-run/react";
-import { isbot } from "isbot";
-import { renderToPipeableStream } from "react-dom/server";
+import type {AppLoadContext, EntryContext} from "@remix-run/node";
+import {createReadableStreamFromReadable} from "@remix-run/node";
+import {RemixServer} from "@remix-run/react";
+import {isbot} from "isbot";
+import {renderToPipeableStream} from "react-dom/server";
 
 const ABORT_DELAY = 5_000;
 
@@ -47,7 +47,7 @@ function handleBotRequest(
 ) {
   return new Promise((resolve, reject) => {
     let shellRendered = false;
-    const { pipe, abort } = renderToPipeableStream(
+    const {pipe, abort} = renderToPipeableStream(
       <RemixServer
         context={remixContext}
         url={request.url}
@@ -97,7 +97,7 @@ function handleBrowserRequest(
 ) {
   return new Promise((resolve, reject) => {
     let shellRendered = false;
-    const { pipe, abort } = renderToPipeableStream(
+    const {pipe, abort} = renderToPipeableStream(
       <RemixServer
         context={remixContext}
         url={request.url}
