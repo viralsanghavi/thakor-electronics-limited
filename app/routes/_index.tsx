aae569872a72e8d62b2b25c3fcde0e1d6b554678
viralsanghavi <vsanghavi3@gmail.com>
2025-01-26 23:57:45 +0530

    feat: fixed home page design

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import type {MetaFunction} from "@remix-run/node";
 import {CompaniesShowcase} from "~/components/CompaniesShowCase";
 import {Footer} from "~/components/Footer";
 import Header from "~/components/Header";
-import HeroSection from "~/components/HeroSection";
+import {ImageSlider} from "~/components/ImageSlider";
 import ProductRange from "~/components/ProductRange";
 
 export const meta: MetaFunction = () => {
@@ -21,11 +21,36 @@ const companies = [
   {name: "Hooli", logo: "https://picsum.photos/id/237/200/300"},
 ];
 
+const slides = [
+  {
+    id: 1,
+    image: "https://picsum.photos/id/237/1200/600/",
+    title: "Welcome to Our Platform",
+    description: "Discover amazing features and boost your productivity.",
+  },
+  {
+    id: 2,
+    image: "https://picsum.photos/id/237/1200/600/",
+    title: "Vision",
+    description:
+      "To be a global leader in passive electronic components, known for quality and innovation. We empower customers with reliable solutions, driving industry advancements and contributing to the global electronics ecosystem from India.",
+  },
+  {
+    id: 3,
+    image: "https://picsum.photos/id/237/1200/600/",
+    title: "Mission",
+    description:
+      "At Thakor Electronics, we lead in high-quality passive electronic components, made with precision in India. We prioritize innovation, sustainability, and reliability, ensuring superior performance for our customers and building lasting global partnerships through ethical practices",
+  },
+];
+
 export default function Index() {
   return (
     <div className="">
       <Header />
-      <HeroSection />
+      <div className="h-full">
+        <ImageSlider slides={slides} />
+      </div>
       <ProductRange />
       <CompaniesShowcase
         title="Trusted by Industry Leaders"
